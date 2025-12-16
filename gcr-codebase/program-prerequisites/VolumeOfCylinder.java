bf965b4cdbf80094875bcc2068ad0fae00c3c612
krishna <krishnagangrade2003@gmail.com>
2025-12-17 00:41:15 +0530

    [Krishna Gangrade] : added VolumeOfCylinder.java

---
+++ gcr-codebase/program-prerequisites/VolumeOfCylinder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+
+public class VolumeOfCylinder{
+    public static void main(String args[]){
+	    Scanner kb= new Scanner(System.in);
+		
+		double radius = kb.nextDouble();
+		double height = kb.nextDouble();
+		double area = Math.PI*radius*radius*height;
+		System.out.println(area);
+	}
+}
+		
