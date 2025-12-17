1d703f3a2325386d2379d35c529d7636031ca03d
krishna <krishnagangrade2003@gmail.com>
2025-12-17 15:50:16 +0530

    [Krishna Gangrade] : added ProfitPercentage.java

---
+++ gcr-codebase/programming-elements/level-1/ProfitPercentage.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ProfitPercentage{
+public static void main(String args[]){
+    
+	int costPrice= 129;
+	int sellingPrice= 191;
+	
+	double profit= sellingPrice - costPrice;
+	
+	double profitPercent= (profit/costPrice)*100;
+	
+	System.out.println("The Cost Price is INR "+costPrice+" and Selling Price is INR "+sellingPrice+"\nThe Profit is INR "+profit+" and the Profit Percentage is "+profitPercent);
+}
+}
+   
