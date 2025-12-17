1f21f89d66f1c245fab822b367268069ce3d1eb9
krishna <krishnagangrade2003@gmail.com>
2025-12-17 15:27:00 +0530

    [Krishna Gangrade] : added KilometersToMiles.java

---
+++ gcr-codebase/programming-elements/level-1/KilometersToMiles.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class KilometersToMiles{
+
+    public static void main(String args[]){
+	    
+		//Creating variable kilometer and assign 10.8 
+		double kilometer= 10.8;
+		
+		
+		//Creating variable miles to indicate the kilometers in miles
+		double miles= 1.6 * kilometer;
+		
+		//Display the result
+		System.out.println("The Distance "+kilometer+"km in miles "+miles);
+	}
+}
+	    
