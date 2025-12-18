9bd69a6abd7e2fb4e0efff8b5d8ec3db623b74bc
krishna <krishnagangrade2003@gmail.com>
2025-12-18 16:04:26 +0530

    [Krishna Gangrade] : added Square Perimeter Calculator

--- gcr-codebase/programming-elements/level-1/SquarePerimeterCalculator.java
+++ gcr-codebase/programming-elements/level-1/SquarePerimeterCalculator.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class SquarePerimeterCalculator{
-public static void main(String args[]){
+    public static void main(String args[]){
 
-    Scanner kb= new Scanner(System.in);
+       Scanner kb= new Scanner(System.in);
 	
-	//Create variable and assign input perimeter
-	double perimeter= kb.nextDouble();
+	    //Create variable and assign input perimeter
+	    double perimeter= kb.nextDouble();
 	
-	//Perimeter of square = 4 * side;
-	double side= perimeter/4;
+	    //Perimeter of square = 4 * side;
+	    double side= perimeter/4;
 	
-	//Displays the result
-	System.out.println("The length of the side is "+side+" whose perimeter is "+perimeter);
-}
+	    //Displays the result
+	    System.out.println("The length of the side is "+side+" whose perimeter is "+perimeter);
+    }
 }
