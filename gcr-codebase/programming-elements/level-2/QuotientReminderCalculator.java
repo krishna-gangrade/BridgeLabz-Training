00becb6a6ab64adf92bcc86ee536237752f383a3
krishna <krishnagangrade2003@gmail.com>
2025-12-18 16:01:48 +0530

    [Krishna Gangrade] : added Quotient Reminder Calculator

--- gcr-codebase/programming-elements/level-2/QuotientReminderCalculator.java
+++ gcr-codebase/programming-elements/level-2/QuotientReminderCalculator.java
@@ -5,12 +5,16 @@ public class QuotientReminderCalculator{
 	public static void main(String args[]){
 		
 		Scanner kb= new Scanner(System.in);
+		
+		//Variables are created to store input
 		int number1= kb.nextInt();
 		int number2= kb.nextInt();
 		
+		//Calculates the quotient and Reminder
 		int quotient= number1/number2;
 		int reminder= number1%number2;
 		
+		//Displays the result
 		System.out.println("The Quotient is "+quotient+" and Reminder is "+reminder+" of two number "+number1+" and "+number2);
 	}
 }
