10d5887092ca7da5ac408450e98162d6c837250b
krishna <krishnagangrade2003@gmail.com>
2025-12-18 13:18:26 +0530

    [Krishna Gangrade] : added DistanceInYardsAndMiles.java

---
+++ gcr-codebase/programming-elements/level-2/DistanceInYardsAndMiles.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DistanceInYardsAndMiles{
+    public static void main(String args[]){
+    
+	    Scanner kb= new Scanner(System.in);
+	
+	    double feet= kb.nextDouble();
+	
+	    double yards= feet/3;
+	
+	    double miles = yards/1760;
+	
+	    System.out.println("The total miles and yards are "+miles+" miles and "+yards+" yards for the given "+feet+" feet");
+    }
+}
