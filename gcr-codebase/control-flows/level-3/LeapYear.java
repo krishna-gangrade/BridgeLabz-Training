0d40ac0c6ebf13aafaf285b84fb48787ef8058c9
krishna <krishnagangrade2003@gmail.com>
2025-12-19 16:34:11 +0530

    [Krishna Gangrade] : added Leap Year Program

---
+++ gcr-codebase/control-flows/level-3/LeapYear.java
@@ -0,0 +1,40 @@
+//Create a class LeapYear and to find if the year is leap year 
+import java.util.*;
+
+public class LeapYear{
+	public static void main(String args[]){
+		
+		Scanner input= new Scanner(System.in);
+		
+		int year= input.nextInt();
+		
+		//LeapYear program only works for year >= 1582
+		if(year>=1582){
+		
+			
+			if(year%400==0){
+				System.out.println(Year+" is a leap Year");
+			}
+			else if(year%100==0){
+				System.out.println(Year+" is not a leap Year");
+			}
+			else if(year%4==0){
+				System.out.println(Year+" is a leap Year");
+			}
+			else{
+				System.out.println(Year+" is not a leap Year");
+			}
+			
+			if((year%4==0 && year%100!=0) ||(year%400==0)){
+				System.out.println(Year+" is a leap Year");
+			}
+			else{
+				System.out.println(Year+" is not a leap Year");
+			}
+			
+		}
+		else{
+			System.out.println("Leap Year calculation is valid only for year >= 1582");
+		}
+	}
+}
