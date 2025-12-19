df44586cd332f106a3a5cc4be9c819940d7395ba
krishna <krishnagangrade2003@gmail.com>
2025-12-19 11:58:28 +0530

    [Krishna Gangrade] : added Fizz Buzz Program

---
+++ gcr-codebase/control-flows/level-2/FizzBuzz.java
@@ -0,0 +1,32 @@
+//Create a class FizzBuzz and print the result
+import java.util.*;
+
+public class FizzBuzz{
+	public static void main(String args[]){
+		
+		Scanner kb= new Scanner(System.in);
+		
+		int number= kb.nextInt();
+		
+		//Condition checks if the number is positive 
+		if(number>0){
+			//for is used to iterate till number
+			for(int i=1;i<=number;i++){
+				
+				//condition checks the divisibility and prints the result
+				if(i%3==0 && i%5==0){
+					System.out.println("FizzBuzz");
+				}
+				else if(i%3==0){
+					System.out.println("Fizz");
+				}
+				else if(i%5==0){
+					System.out.println("Buzz");
+				}else{
+					System.out.println(i);
+				}
+			}
+		}
+	}
+}
+			
