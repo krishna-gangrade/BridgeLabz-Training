86fbf32d3faa44e2008fe0637f41aaea09e8ddfd
krishna <krishnagangrade2003@gmail.com>
2025-12-19 16:40:42 +0530

    [Krishna Gangrade] : added Leap Year Using Logical Operator Program

---
+++ gcr-codebase/control-flows/level-3/LeapYearUsingLogicalOperator.java
@@ -0,0 +1,27 @@
+//Create a class LeapYear and to find if the year is leap year 
+import java.util.*;
+
+public class LeapYearUsingLogicalOperator{
+	public static void main(String args[]){
+		
+		Scanner input= new Scanner(System.in);
+		
+		int year= input.nextInt();
+		
+		//LeapYear program only works for year >= 1582
+		if(year>=1582){
+			
+			//Condition using logical and && and or || operators
+			if((year%4==0 && year%100!=0) ||(year%400==0)){
+				System.out.println(Year+" is a leap Year");
+			}
+			else{
+				System.out.println(Year+" is not a leap Year");
+			}
+			
+		}
+		else{
+			System.out.println("Leap Year calculation is valid only for year >= 1582");
+		}
+	}
+}
