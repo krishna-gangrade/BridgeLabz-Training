613ec89b0b5c52ea0382dcf3d2d5da9730529a6c
krishna <krishnagangrade2003@gmail.com>
2025-12-20 08:36:06 +0530

    [Krishna Gangrade] : added Abundant Number Program

--- gcr-codebase/control-flows/level-3/CalculatorUsingSwich.java
+++ gcr-codebase/control-flows/level-3/CalculatorUsingSwich.java
@@ -1,14 +1,4 @@
-Write a program to create a calculator using switch...case.
-Hint => 
-Create two double variables named first and second and a String variable named op.
-Get input values for all variables.
-The input for the operator can only be one of the four values: "+", "-", "*" or "/".
-Run a for loop from i = 1 to i < number.
-Based on the input value of the op, perform specific operations using the switch...case statement and print the result.
-If op is +, perform addition between first and second; if it is -, perform subtraction and so on.
-If op is neither of those 4 values, print Invalid Operator.
-
-
+//Create a class CalculatorUsingSwitch 
 import java.util.*;
 
 public class CalculatorUsingSwitch{
