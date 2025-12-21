90eb0c7f0040f336905cca7dfc56813e79ef36fd
krishna <krishnagangrade2003@gmail.com>
2025-12-21 22:00:14 +0530

    [Krishna Gangrade] : added Extended Unit Converter Program

---
+++ gcr-codebase/methods/level-2/ExtendedUnitConverter.java
@@ -0,0 +1,43 @@
+//Create a utility class ExtendedUnitConvertor with static conversion methods.
+public class ExtendedUnitConvertor {
+
+    //Convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    //Convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    //Convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    //Convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    //Convert inches to centimeters
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    //main method
+    public static void main(String[] args) {
+
+        System.out.println("10 yards in feet = " + convertYardsToFeet(10));
+        System.out.println("10 feet in yards = " + convertFeetToYards(10));
+        System.out.println("10 meters in inches = " + convertMetersToInches(10));
+        System.out.println("10 inches in meters = " + convertInchesToMeters(10));
+        System.out.println("10 inches in centimeters = " + convertInchesToCentimeters(10));
+    }
+}
