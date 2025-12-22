05402d4e4654c7981ea9cdea088fd518ffa648ec
krishna <krishnagangrade2003@gmail.com>
2025-12-22 09:03:31 +0530

    [Krishna Gangrade] : added Collinear Points Program

---
+++ gcr-codebase/methods/level-3/CollinearPoints.java
@@ -0,0 +1,30 @@
+//Created class to check collinear points
+import java.util.*;
+public class CollinearPoints{
+
+    public static boolean isCollinearBySlope(int x1,int y1,int x2,int y2,int x3,int y3){
+        double slopeAB=(double)(y2-y1)/(x2-x1);
+        double slopeBC=(double)(y3-y2)/(x3-x2);
+        double slopeAC=(double)(y3-y1)/(x3-x1);
+        return slopeAB==slopeBC && slopeBC==slopeAC;
+    }
+
+    public static boolean isCollinearByArea(int x1,int y1,int x2,int y2,int x3,int y3){
+        double area=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+        return area==0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+        int x3=sc.nextInt();
+        int y3=sc.nextInt();
+
+        System.out.println(isCollinearBySlope(x1,y1,x2,y2,x3,y3));
+        System.out.println(isCollinearByArea(x1,y1,x2,y2,x3,y3));
+    }
+}
