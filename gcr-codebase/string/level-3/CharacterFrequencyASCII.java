57959901bcf064908225ec6f8153511a012f077f
krishna <krishnagangrade2003@gmail.com>
2025-12-23 16:23:53 +0530

    [Krishna Gangrade] : added Character Frequency ASCII Program

--- gcr-codebase/string/level-3/CharacterFrequencyASCII.java
+++ gcr-codebase/string/level-3/CharacterFrequencyASCII.java
@@ -1,59 +1,30 @@
 import java.util.Scanner;
 
-//Create class CharacterFrequencyASCII
+// Create class CharacterFrequencyASCII
 public class CharacterFrequencyASCII {
 
+    // Method to find frequency of characters using ASCII
     public static String[][] findFrequency(String text) {
         int[] freq = new int[256];
 
+        // Count frequency
         for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i)!=' ')
             freq[text.charAt(i)]++;
         }
 
+        // Result array (max size = length of text)
         String[][] result = new String[text.length()][2];
         int index = 0;
 
+        // Store character and frequency only once
         for (int i = 0; i < text.length(); i++) {
             char ch = text.charAt(i);
+
             if (freq[ch] > 0) {
-                result[index]import java.util.Scanner;
-
-public class FrequencyUsingUniqueCharacters {
-
-    public static char[] uniqueCharacters(String text) {
-        char[] unique = new char[text.length()];
-        int index = 0;
-
-        for (int i = 0; i < text.length(); i++) {
-            boolean isUnique = true;
-            for (int j = 0; j < i; j++) {
-                if (text.charAt(i) == text.charAt(j)) {
-                    isUnique = false;
-                    break;
-                }
-            }
-            if (isUnique) {
-                unique[index++] = text.charAt(i);
-            }
-        }
-        return unique;
-    }
-
-    public static String[][] findFrequency(String text) {
-        int[] freq = new int[256];
-
-        for (int i = 0; i < text.length(); i++) {
-            freq[text.charAt(i)]++;
-        }
-
-        char[] uniqueChars = uniqueCharacters(text);
-        String[][] result = new String[uniqueChars.length][2];
-
-        int index = 0;
-        for (char ch : uniqueChars) {
-            if (ch != '\0') {
                 result[index][0] = String.valueOf(ch);
                 result[index][1] = String.valueOf(freq[ch]);
+                freq[ch] = 0; // mark as processed
                 index++;
             }
         }
@@ -62,40 +33,18 @@ public class FrequencyUsingUniqueCharacters {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter text: ");
         String text = sc.nextLine();
 
         String[][] freq = findFrequency(text);
 
-        System.out.println("Character Frequency:");
+        System.out.println("\nCharacter Frequency:");
         for (String[] row : freq) {
             if (row[0] != null) {
                 System.out.println(row[0] + " : " + row[1]);
             }
         }
-    }
-}
-[0] = String.valueOf(ch);
-                result[index][1] = String.valueOf(freq[ch]);
-                freq[ch] = 0;
-                index++;
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter text: ");
-        String text = sc.nextLine();
-
-        String[][] freq = findFrequency(text);
-
-        System.out.println("Character Frequency:");
-        for (String[] row : freq) {
-            if (row[0] != null) {
-                System.out.println(row[0] + " : " + row[1]);
-            }
-        }
+        sc.close();
     }
 }
