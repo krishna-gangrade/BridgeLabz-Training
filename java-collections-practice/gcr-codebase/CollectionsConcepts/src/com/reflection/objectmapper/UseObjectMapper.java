89e73366d0affe393230f51fc2295d17680e9bca
krishna <krishnagangrade2003@gmail.com>
2026-01-27 11:50:11 +0530

    [Krishna Gangrade] : added Create a Custom Object Mapper Problem Java Files.

---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/objectmapper/UseObjectMapper.java
@@ -0,0 +1,24 @@
+
+package com.reflection.objectmapper;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class UseObjectMapper {
+    public static void main(String[] args) {
+	Map<String, Object> data = new HashMap<>();
+
+	data.put("name", "Pritam");
+	data.put("age", 22);
+
+	try {
+	    User user = ObjectMapper.toObject(User.class, data);
+
+	    System.out.println("Name: " + user.getName());
+	    System.out.println("Age : " + user.getAge());
+	} catch (Exception e) {
+
+	    System.out.println("Mapping failed.");
+	}
+    }
+}
