e570ff7cdafcd1df95eb802cef9c7cd4e3c4db9f
krishna <krishnagangrade2003@gmail.com>
2026-01-27 11:48:46 +0530

    [Krishna Gangrade] : added Dynamic Method Invocation Problem Java Files.

---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/dynamicmethod/MathOperations.java
@@ -0,0 +1,17 @@
+
+package com.reflection.dynamicmethod;
+
+public class MathOperations {
+
+    public int add(int a, int b) {
+	return a + b;
+    }
+
+    public int subtract(int a, int b) {
+	return a - b;
+    }
+
+    public int multiply(int a, int b) {
+	return a * b;
+    }
+}
