f349867a97cb9d953294e6af3332dbddcc18e62d
krishna <krishnagangrade2003@gmail.com>
2026-01-27 11:49:08 +0530

    [Krishna Gangrade] : added Dynamically Create Objects Problem Java Files.

---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/dynamicobject/DynamicObjectCreation.java
@@ -0,0 +1,16 @@
+
+package com.reflection.dynamicobject;
+
+public class DynamicObjectCreation {
+    public static void main(String[] args) {
+	try {
+	
+	    Class<?> clazz = Class.forName("com.reflection.dynamicobject.Student");
+
+	    Object obj = clazz.getDeclaredConstructor().newInstance();
+
+	} catch (Exception e) {
+	    System.out.println("Creation failed.");
+	}
+    }
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/dynamicobject/Student.java
@@ -0,0 +1,9 @@
+
+package com.reflection.dynamicobject;
+
+public class Student {
+    // constructor
+    public Student() {
+	System.out.println("Student object created.");
+    }
+}
