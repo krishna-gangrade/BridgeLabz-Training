ddbc507585deedba863389de965130c8f2ffe750
krishna <krishnagangrade2003@gmail.com>
2026-01-27 11:51:32 +0530

    [Krishna Gangrade] : added Custom Logging Proxy Using Reflection Problem Java Files.

---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/proxy/Greeting.java
@@ -0,0 +1,7 @@
+
+package com.reflection.proxy;
+
+public interface Greeting {
+    // method to be overridden by child class
+    void sayHello();
+}
---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/proxy/GreetingImplementation.java
@@ -0,0 +1,11 @@
+
+package com.reflection.proxy;
+
+public class GreetingImplementation implements Greeting {
+
+    // Overridden Parent's method
+    @Override
+    public void sayHello() {
+        System.out.println("Hello User!");
+    }
+}
