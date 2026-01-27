cd46464cd68ddc0493bece6bdca97cc5a71cd1d6
krishna <krishnagangrade2003@gmail.com>
2026-01-27 11:49:50 +0530

    [Krishna Gangrade] : added Generate a JSON Representation Problem Java Files.

---
+++ java-collections-practice/gcr-codebase/CollectionsConcepts/src/com/reflection/jsongenerator/User.java
@@ -0,0 +1,13 @@
+
+package com.reflection.jsongenerator;
+
+public class User {
+    private String name;
+    private int age;
+
+    // constructor
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
