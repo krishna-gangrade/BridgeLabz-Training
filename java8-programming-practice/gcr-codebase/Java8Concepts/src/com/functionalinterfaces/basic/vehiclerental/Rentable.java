22c77598cb8142087cfab2a0ab91cfd7fe900fc8
krishna <krishnagangrade2003@gmail.com>
2026-01-30 13:11:20 +0530

    [Krishna Gangrade] : added Multi-Vehicle Rental System Problem Java Files.

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/basic/vehiclerental/Rentable.java
@@ -0,0 +1,9 @@
+package com.functionalinterfaces.basic.vehiclerental;
+
+public interface Rentable {
+    // rents the vehicle for the given number of hours
+    void rent(int hours);
+
+    // returns the vehicle and shows total cost
+    void returnVehicle();
+}
