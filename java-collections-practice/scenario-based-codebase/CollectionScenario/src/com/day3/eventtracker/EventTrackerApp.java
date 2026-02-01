3c8d2ba324b632bf203b8c35027f3bf436c87385
krishna <krishnagangrade2003@gmail.com>
2026-02-02 00:58:52 +0530

    [Krishna Gangrade] : added EventTracker Scenario-Based Problem Java Files.

---
+++ java-collections-practice/scenario-based-codebase/CollectionScenario/src/com/day3/eventtracker/EventTrackerApp.java
@@ -0,0 +1,33 @@
+package com.day3.eventtracker;
+
+
+package com.day3.eventtracker;
+
+import com.day3.eventtracker.model.AuditEvent;
+import com.day3.eventtracker.service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.util.List;
+
+public class EventTrackerApp {
+	public static void main(String[] args) {
+		EventTracker tracker = new EventTracker();
+		tracker.scanClass(UserService.class);
+		List<AuditEvent> events = tracker.getEvents();
+
+		try {
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			FileWriter writer = new FileWriter("samplefiles/eventtracker/audit_log.json");
+			gson.toJson(events, writer);
+
+			writer.flush();
+			writer.close();
+			System.out.println("Audit log generated successfully.");
+			
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
