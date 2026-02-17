b74e2a86942260a8a3f0f1110b6479228f14fc88
krishna <krishnagangrade2003@gmail.com>
2026-02-17 11:43:23 +0530

    [Krishna Gangrade] : added Java files for Connection pooling.

--- dbms-jdbc-practice/gcr-codebase/DBMSJDBCConcepts/src/main/java/com/jdbc/connectionpooling/ConnectionPooling.java
+++ dbms-jdbc-practice/gcr-codebase/DBMSJDBCConcepts/src/main/java/com/jdbc/connectionpooling/ConnectionPooling.java
@@ -12,7 +12,7 @@ public class ConnectionPool {
 		HikariConfig config = new HikariConfig();
 		config.setJdbcUrl("jdbc:mysql://localhost:3306/mydb");
 		config.setUsername("root");
-		config.setPassword("root");
+		config.setPassword("2003");
 
 // Pool configuration
 		config.setMaximumPoolSize(10);
