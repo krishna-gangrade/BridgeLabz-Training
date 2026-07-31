3490a1a2ed07c0c0af88e83e97ab0013578f2b72
krishna <krishnagangrade2003@gmail.com>
2026-07-31 18:18:00 +0530

    [Krishna Gangrade] : added program of array manipulation

---
+++ Refresher-Training/src/Day_01/HackerRank-based-codebase/ArrayManipulation/arrayManipulation.java
@@ -0,0 +1,73 @@
+package Day_01.ArrayManipulation;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+
+        long[] arr= new long[n+2];
+
+        for(List<Integer> query :queries){
+            int a =query.get(0);
+            int b =query.get(1);
+            int k =query.get(2);
+
+            arr[a]+=k;
+
+            if(b+1<=n){
+                arr[b+1]-=k;
+            }
+        }
+
+        long max =0;
+        long sum =0;
+
+        for(int i=1;i<=n;i++){
+            sum +=arr[i];
+
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+
+}
+
+public class arrayManipulation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
