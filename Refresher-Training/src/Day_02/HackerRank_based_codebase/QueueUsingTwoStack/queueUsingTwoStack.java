e5cd8e15e196be4c4cb618272f8a6d3cdb9571a0
krishna <krishnagangrade2003@gmail.com>
2026-08-04 15:29:26 +0530

    [Krishna Gangrade] : added program for Queue Using Two Stacks

--- Refresher-Training/src/Day_02/HackerRank_based_codebase/QueueUsingTwoStack/queueUsingTwoStack.java
+++ Refresher-Training/src/Day_02/HackerRank_based_codebase/QueueUsingTwoStack/queueUsingTwoStack.java
@@ -1,4 +1,36 @@
 package Day_02.HackerRank_based_codebase.QueueUsingTwoStack;
 
-public class queueUsingTwoStack {
+import java.util.*;
+
+public class queueUsingTwoStack{
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        Stack<Integer> input = new Stack<>();
+        Stack<Integer> output = new Stack<>();
+
+        for(int i =0;i<n;i++){
+            int op =kb.nextInt();
+            if(op ==1){
+                input.push(kb.nextInt());
+            }else if(op ==2){
+                if(output.empty()){
+                    while(!input.empty()){
+                        output.push(input.pop());
+                    }
+                }
+                output.pop();
+            }else if(op ==3){
+                if(output.empty()){
+                    while(!input.empty()){
+                        output.push(input.pop());
+                    }
+                }
+                System.out.println(output.peek());
+            }
+        }
+        kb.close();
+    }
 }
