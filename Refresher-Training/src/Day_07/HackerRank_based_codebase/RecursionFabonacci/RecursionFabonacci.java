fb099466b5d2be4da4fa7dcdc373f30e24e6c59c
krishna <krishnagangrade2003@gmail.com>
2026-08-10 18:07:00 +0530

    [Krishna Gangrade] : added Fabonacci Problem By Recursion

--- Refresher-Training/src/Day_07/HackerRank_based_codebase/RecursionFabonacci/RecursionFabonacci.java
+++ Refresher-Training/src/Day_07/HackerRank_based_codebase/RecursionFabonacci/RecursionFabonacci.java
@@ -1,4 +1,25 @@
 package Day_07.HackerRank_based_codebase.RecursionFabonacci;
 
+import java.util.*;
+
 public class RecursionFabonacci {
+
+    public static int fibonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
 }
