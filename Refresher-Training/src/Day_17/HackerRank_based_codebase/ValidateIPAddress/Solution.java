6dc61f57630117529b9ed02b6e9e3936ac4963b1
krishna <krishnagangrade2003@gmail.com>
2026-08-24 18:05:00 +0530

    [Krishna Gangrade] : added Validate IP Address Program

---
+++ Refresher-Training/src/Day_17/HackerRank_based_codebase/ValidateIPAddress/Solution.java
@@ -0,0 +1,19 @@
+package Day_17.HackerRank_based_codebase.ValidateIPAddress;
+
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    String pattern ="((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"+"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+}
